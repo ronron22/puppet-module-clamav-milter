c3c9e02b2dfea0657d531462aa16cc27be4836de
ronron <ant@n.io>
2018-09-24 21:26:42 +0200

    .

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,7 +9,7 @@ class clamavmilter::config (
     file { $configuration_directory :
       ensure => present,
       sourceselect => all,
-      source => ["puppet:///modules/clamavmilter/${nodename}/etc/clamav-milter","/etc/clamavmilter "],
+      source => ["puppet:///modules/clamavmilter/${nodename}/etc/clamav-milter","/etc/clamav-milter "],
       recurse => true,
       notify   => Service[$service_name],  
     }
